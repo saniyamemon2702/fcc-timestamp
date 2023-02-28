7857de09810723f7c6aa938594f6419ef9ebc47e
saniyamemon2702 <saniyamemon7862@gmail.com>
2023-02-28 18:03:37 -0500

    Task Completed!

--- index.js
+++ index.js
@@ -5,10 +5,8 @@
 var express = require('express');
 var app = express();
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -18,11 +16,16 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
 // your first API endpoint... 
 app.get("/api/:date", function (req, res) {
-  console.log(req.params.date);
-  res.json({utc: new Date(req.params.date).toUTCString()});
+  console.log( `${new Date(parseInt(req.params.date))} ${ new Date(parseInt(req.params.date))!= "Invalid Date"}`);
+   if(new Date(parseInt(req.params.date))!= "Invalid Date"){
+ 
+  res.json({ unix: new Date(parseInt(req.params.date)).getTime(), utc: new Date(parseInt(req.params.date)).toUTCString() });
+}else {
+  console.log("hellos");
+  res.json({error:"Invalid Date"});}
+  
 });
 
 
